eb4f46d5baa912ffaeaf5159d6525e4fce773c45
Biswajit Patgiri <biswajitpatgiri41@gmail.com>
2026-07-27 17:43:05 +0530

    Solve sarsa-update

---
+++ sarsa-update/sarsa-update.py
@@ -0,0 +1,5 @@
+def sarsa_update(q_table, state, action, reward, next_state, next_action, alpha, gamma):
+    new_q = [row[:] for row in q_table]
+    td = reward + gamma * q_table[next_state][next_action] - q_table[state][action]
+    new_q[state][action] += alpha * td
+    return new_q
