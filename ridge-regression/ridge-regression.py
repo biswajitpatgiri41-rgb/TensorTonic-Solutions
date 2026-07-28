ee1c85429d14f30efe902465ddc162042071ced5
Biswajit Patgiri <biswajitpatgiri41@gmail.com>
2026-07-28 18:45:53 +0530

    Solve ridge-regression

---
+++ ridge-regression/ridge-regression.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def ridge_regression(X, y, lam):
+    X = np.array(X, dtype=float)
+    y = np.array(y, dtype=float)
+    d = X.shape[1]
+    I = np.eye(d)
+    w = np.linalg.inv(X.T @ X + lam * I) @ (X.T @ y)
+    return w.tolist()
