b27b0907ddcf5c47f666cf2ecf62d0bc12586ab5
Biswajit Patgiri <biswajitpatgiri41@gmail.com>
2026-08-01 16:57:28 +0530

    Solve weighted-moving-average

---
+++ weighted-moving-average/weighted-moving-average.py
@@ -0,0 +1,8 @@
+def weighted_moving_average(values, weights):
+    k = len(weights)
+    w_sum = sum(weights)
+    result = []
+    for i in range(len(values) - k + 1):
+        weighted_sum = sum(weights[j] * values[i + j] for j in range(k))
+        result.append(weighted_sum / w_sum)
+    return result
